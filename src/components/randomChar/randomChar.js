b9f5111ff47126cfa3807832eaad4c037fd5d761
Svyatoslav <slavaanufriev@gmail.com>
2023-07-25 20:48:46 +0300

    randomChat html structure

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,11 +1,37 @@
+import './randomChar.scss';
+import thor from '../../resources/img/thor.png';
+import mjolnir from '../../resources/img/mjolnir.png';
 
 
 
 function RandomChar() {
     return(
-        <>
-        Hello world
-        </>
+        <div className="randomchar">
+            <div className="randomchar__block">
+                <img src={thor} alt="thor"/>
+                <div className="randomchar__info">
+                    <p className="randomchar__name">THOR</p>
+                    <p className="randomchar__descr">As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...</p>
+                    <div className="randomchar__btns">
+                        <a href="#">
+                            <div className="inner">HOMEPAGE</div>
+                        </a>
+                        <a href="#">
+                            <div className="inner">WIKI</div>
+                        </a>
+                    </div>
+                </div>
+                <div className="randomchar__static">
+                    <p className="randomchar__title">Random character for today!<br/>Do you want to get to know him better?</p>
+                    <p className="randomchar__title">Or choose another one</p>
+                    <a href="#">
+                            <div className="inner">TRY IT</div>
+                    </a>
+                    <img src={mjolnir} alt="mjolnir" />   
+                </div>
+
+            </div>
+        </div>
     );
 }
 
