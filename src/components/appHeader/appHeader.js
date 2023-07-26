8bbe8eeaad7781a792e2a3bee70064f8553cc0f3
Svyatoslav <slavaanufriev@gmail.com>
2023-07-26 18:41:31 +0300

    appHeader

--- src/components/appHeader/appHeader.js
+++ src/components/appHeader/appHeader.js
@@ -1,9 +1,17 @@
-
+import "./appHeader.scss";
 
 function AppHeader() {
     return(
-        <>
-        </>
+        <header className="app__header">
+            <h1 className="app__title"><span>Marvel</span> information portal</h1>
+            <nav className="app__menu">
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    /
+                    <li><a href="#"></a>Comics</li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
