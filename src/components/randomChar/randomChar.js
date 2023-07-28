1f4f9ccd8c73e203d5eb8d207eedd5bdeab5c997
Svyatoslav <slavaanufriev@gmail.com>
2023-07-28 21:46:09 +0300

    getting of info about a random char

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,37 +1,64 @@
+import { Component } from 'react';
+
 import './randomChar.scss';
-import thor from '../../resources/img/thor.png';
 import mjolnir from '../../resources/img/mjolnir.png';
+import MarvelService from '../../services/MarvelService';
 
 
 
-function RandomChar() {
-    return(
-        <div className="randomchar">
-            <div className="randomchar__block">
-                <img className='randomchar__img' src={thor} alt="thor"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">THOR</p>
-                    <p className="randomchar__descr">As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...</p>
-                    <div className="randomchar__btns">
-                        <a className='button button__main' href="#">
-                            <div className="inner">HOMEPAGE</div>
-                        </a>
-                        <a className="button button__secondary" href="#">
-                            <div className="inner">WIKI</div>
-                        </a>
+class RandomChar extends Component {
+    state = {
+        char: {}
+    }
+    marvelService = new MarvelService();
+    onCharLoaded = (char) => {
+        this.setState({char})
+    }
+
+    constructor(props){
+        super(props);
+        this.updateChar();
+    }
+
+    updateChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.marvelService.getCharacter(id)
+            .then(this.onCharLoaded);
+    }
+
+    render(){
+        const {name, description, thumbnail, homepage, wiki} = this.state.char;
+
+        return(
+            <div className="randomchar">
+                <div className="randomchar__block">
+                    <div className="randomchar__img">
+                        <img className='randomchar__img' src={thumbnail} alt="thor"/>
+                    </div>
+                    <div className="randomchar__info">
+                        <p className="randomchar__name">{name}</p>
+                        <p className="randomchar__descr">{description}</p>
+                        <div className="randomchar__btns">
+                            <a className='button button__main' href={homepage}>
+                                <div className="inner">HOMEPAGE</div>
+                            </a>
+                            <a className="button button__secondary" href={wiki}>
+                                <div className="inner">WIKI</div>
+                            </a>
+                        </div>
                     </div>
                 </div>
+                <div className="randomchar__static">
+                        <p className="randomchar__title">Random character for today!<br/>Do you want to get to know him better?</p>
+                        <p className="randomchar__title">Or choose another one</p>
+                        <button className="button button__main">
+                            <div className="inner">try it</div>
+                        </button>
+                        <img className='randomchar__decoration' src={mjolnir} alt="mjolnir" />   
+                </div>
             </div>
-            <div className="randomchar__static">
-                    <p className="randomchar__title">Random character for today!<br/>Do you want to get to know him better?</p>
-                    <p className="randomchar__title">Or choose another one</p>
-                    <button className="button button__main">
-                        <div className="inner">try it</div>
-                    </button>
-                    <img className='randomchar__decoration' src={mjolnir} alt="mjolnir" />   
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default RandomChar;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -10,12 +10,31 @@ class MarvelService {
         return await result.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        let res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        if(res.data.results[0].description !== "")
+        {
+            return this._transformCharacter(res.data.results[0]);
+        }
+        else{
+            res = this.getCharacter(Math.floor(Math.random() * (1011400 - 1011000) + 1011000))
+            return res;
+        }
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
     }
 }
 
